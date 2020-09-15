6d166373716fd7fa8cd11ed694235bbc82fa8f5f
Ben Mezger <me@benmezger.nl>
2020-09-14 23:31:46 -0300

    Check mime and filename before renaming

--- rename.py
+++ rename.py
@@ -1,7 +1,14 @@
+import re
 import os
+import mimetypes
 
+FILENAME_REGEX = re.compile("^\d+\.(jpg|jpeg|png)$")
 
 for i, file in enumerate(os.listdir("."), 1):
-    if file.endswith("png"):
+    if FILENAME_REGEX.search(file):
+        continue
+
+    mime, _ = mimetypes.guess_type(file)
+    if mime == "image/png" or mime == "image/jpeg":
         size = file.split("_")[-1]
         os.rename(file, f"{i}-{size}")
